7ddbf380f5809cff757d10510968d220b2477458
Elixr2 <ij553504@gmail.com>
2026-05-19 18:57:53 +0900

    scriptMove

--- Prototype/Assets/02. Script/PlayerMove.cs
+++ Prototype/Assets/02. Script/PlayerMove.cs
@@ -5,10 +5,14 @@ public class PlayerMove : MonoBehaviour
     Transform tr;
     Rigidbody2D rb;
     public float speed = 10.0f;
+    public Vector2 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
     void Awake()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        jump = new Vector2(0.0f, 2.0f);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Update is called once per frame
@@ -17,14 +21,21 @@ public class PlayerMove : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); //a 일떄 -1, d 일때 1, 그외 0
         float v = Input.GetAxis("Vertical"); //w 일떄 1, s 일떄 -1, 그외 0 (우리 게임에서는 점프 외에는 y축 이동이 없으므로 v는 사용하지 않음)
-        Move(h, 0); //a와 d키로 좌우 이동, y축 이동은 없으므로 0을 전달
-
+        rb.linearVelocity = new Vector2(h * speed, rb.linearVelocity.y); //a와 d키로 좌우 이동, y축 이동은 없으므로 0을 전달
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
 
-    void Move(float x, float y)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 p = (Vector2)tr.position + new Vector2(x, y) * speed * Time.deltaTime; //현재 위치에서 입력값과 속도, 프레임 시간을 곱해서 이동할 위치를 계산
-        //Time.deltaTime -> move 함수가 void Update()에서 매 프레임마다 호출되므로, 컴퓨터가 좋아져 프레임이 높아질수록 이동이 빨라지는 것을 방지
-        rb.MovePosition(p);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
-}
+} 
+
+    
