68c1dbffa7953d74396c9086e991dbf9e9e2eac8
Elixr2 <ij553504@gmail.com>
2026-05-26 18:00:13 +0900

    Stage Portal Code

---
+++ Prototype/Assets/02. Script/Portal_next.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Portal_next : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] private string nextSceneName;
+    private bool playerInPortal = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    {
+        if (other.CompareTag("Player"))
+            {
+                playerInPortal = true;
+                Debug.Log("Player entered portal zone");
+            }
+    }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInPortal = false;
+            Debug.Log("Plater left portal zone");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInPortal && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SceneManager.LoadScene(nextSceneName);
+            print("Success");
+        }
+    }
+
+
+}
+    
